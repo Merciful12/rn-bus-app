452538b3c52f1a41a93ece7fdff56f39f5282928
Stanislav <future.12@yandex.ru>
2019-04-12 22:58:32 +0300

    refactor to func component

--- App.tsx
+++ App.tsx
@@ -8,27 +8,18 @@
  * @format
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, {useState} from 'react'
+import {StyleSheet, Text, View, Button} from 'react-native'
 
 interface Props {}
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default function App ({}: Props) {
+  const [count, setCount] = useState(0)
+  return (
+    <View style={styles.container}>
+      <Text style={styles.instructions}>Count - {count}</Text>
+      <Button title="Click me" onPress={() => setCount(count + 1)} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -38,14 +29,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
