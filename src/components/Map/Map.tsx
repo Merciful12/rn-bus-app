693a577823f6717e8b8d061bbe56965aad3b59db
Stanislav <future.12@yandex.ru>
2019-05-09 18:47:54 +0300

    created map component

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,71 @@
+import React, { FC } from 'react'
+import { StyleSheet, ImageStyle, Image } from 'react-native'
+import MapView, { Marker, MapEvent, Callout } from 'react-native-maps'
+import { IBusstop } from '../../graphql/queries'
+
+const stationIcon = require('../../assets/icons/station.png')
+
+interface IProps {
+  busstops: IBusstop[],
+  onPress: (e: MapEvent) => void,
+}
+
+const Map: FC<IProps> = ({busstops, onPress}) => (
+  <MapView
+    style={styles.map}
+    onMarkerPress={onPress}
+    customMapStyle={customStylesMap}
+    region={region}
+    initialRegion={region}
+  >
+    {busstops.map(busstop => (
+      <Marker
+        tracksViewChanges={false}
+        identifier={`${busstop.id}`}
+        key={busstop.id}
+        coordinate={{
+          latitude: busstop.lat,
+          longitude: busstop.lon
+        }}
+      >
+        <Image source={stationIcon} style={styles.marker} />
+        <Callout tooltip />
+      </Marker>
+    ))}
+  </MapView>
+)
+
+const region = {
+  latitude: 51.67204,
+  longitude: 39.1843,
+  latitudeDelta: 0.015,
+  longitudeDelta: 0.0121 
+}
+
+interface IStyles {
+  map: any,
+  marker: ImageStyle,
+}
+
+const styles = StyleSheet.create<IStyles>({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  marker: {
+    width: 40,
+    height: 25,
+  }
+})
+
+const customStylesMap = [
+  {
+    "featureType": "transit.station.bus",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
+
+export default Map
