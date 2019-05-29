c2424851b6b30ed4821cc7780283f8cf9144b7d9
Stanislav <future.12@yandex.ru>
2019-05-29 14:41:09 +0300

    added react native screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { ApolloProvider } from 'react-apollo-hooks'
 import {Provider} from 'react-redux'
+import { useScreens } from 'react-native-screens'
 
 import store from './redux'
 import { client } from './graphql'
 import AppNavigation from './navigator'
+
+
+useScreens()
 
 const App = () => (
   <Provider store={store}>
